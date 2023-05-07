19bab4fabfd00290b7e541757bc37c9afbd7f4b3
MathieuKoz <127348750+MathieuKoz@users.noreply.github.com>
2023-05-07 20:27:44 +0200

    Combinaison V2
    modification verification suite, prise en compte 4 cartes

--- Combinaison.cpp
+++ Combinaison.cpp
@@ -2,7 +2,7 @@
 
 
 
-bool Combinaison::estUneCouleur(){
+bool Combinaison::estUneCouleur3(){
     Couleur C1= c1->get_couleur();
     Couleur C2= c2->get_couleur();
     Couleur C3= c3->get_couleur();
@@ -10,15 +10,47 @@ bool Combinaison::estUneCouleur(){
     return (C1==C2 && C2==C3);
 }
 
-bool Combinaison::estUneSuite(){
+bool Combinaison::estUneCouleur4(){
+    Couleur C1= c1->get_couleur();
+    Couleur C2= c2->get_couleur();
+    Couleur C3= c3->get_couleur();
+    Couleur C4= c4->get_couleur();
+
+    return (C1==C2 && C2==C3 && C3==C4);
+}
+
+
+
+bool Combinaison::estUneSuite3(){
     int v1=c1->get_force();
     int v2=c2->get_force();
     int v3=c3->get_force();
 
-    return  (v1*2==v2+v3-2 || v2*2==v1+v3-2 || v3*2== v1+v2-2);
+    bool r1,r2;
+    r1=(v1*2==v2+v3-3 || v2*2==v1+v3-3 || v3*2== v1+v2-3);
+    r2=(v1!=v2 && v1!=v3 && v2!= v3);
+
+    return (r1 && r2);
 }
 
-bool Combinaison::estUnBrelan(){
+bool Combinaison::estUneSuite4(){
+    int v1=c1->get_force();
+    int v2=c2->get_force();
+    int v3=c3->get_force();
+    int v4=c4->get_force();
+    
+    bool r1, r2;
+    r1 = (v1*3==v2+v3+v4-6 || v2*3==v1+v3+v4-6 || v3*3== v1+v2+v4-6 || v4*3== v1+v2+v3-6);
+    r2 = (v1!= v2 && v1!= v3 && v1!= v4 && v2!= v3 && v2!= v4 && v3!= v4);
+
+    return (r1 && r2);
+}
+
+
+
+
+
+bool Combinaison::estUnBrelan3(){
     int v1=c1->get_force();
     int v2=c2->get_force();
     int v3=c3->get_force();
@@ -26,17 +58,51 @@ bool Combinaison::estUnBrelan(){
     return (v1==v2 && v2==v3);
 }
 
-bool Combinaison::estUneSuiteCouleur(){
-    return (estUneCouleur() && estUneSuite());
+bool Combinaison::estUnBrelan4(){
+    int v1=c1->get_force();
+    int v2=c2->get_force();
+    int v3=c3->get_force();
+    int v4=c4->get_force();
+
+    return (v1==v2 && v2==v3 && v3==v4);
 }
+
+
+bool Combinaison::estUneSuiteCouleur3(){
+    return (estUneCouleur3() && estUneSuite3());
+}
+bool Combinaison::estUneSuiteCouleur4(){
+    return (estUneCouleur4() && estUneSuite4());
+}
+
+
+
 
 void Combinaison::PuissanceCombinaison(){
-    int p=c1->get_force() + c2->get_force() + c3->get_force();
+    if (nb_carte==3) {
+        
+        int p=c1->get_force() + c2->get_force() + c3->get_force();
 
-    if (estUneSuiteCouleur()) p=p+10000;
-    if (estUnBrelan()) p=p+10000;
-    if (estUneCouleur()) p=p+1000;
-    if (estUneSuite()) p=p+100;
+        if (estUneSuiteCouleur3()) p=p+10000;
+        if (estUnBrelan3()) p=p+10000;
+        if (estUneCouleur3()) p=p+1000;
+        if (estUneSuite3()) p=p+100;
+        Puissance=p;
+    }
+    if (nb_carte==4) {
+        
+        int p=c1->get_force() + c2->get_force() + c3->get_force() + c4->get_force();
 
-    Puissance=p;
-}
+        if (estUneSuiteCouleur4()) p=p+10000;
+        if (estUnBrelan4()) p=p+10000;
+        if (estUneCouleur4()) p=p+1000;
+        if (estUneSuite4()) p=p+100;
+        Puissance=p;
+    }
+
+
+
+
+    
+};
+
--- Combinaison.h
+++ Combinaison.h
@@ -7,19 +7,34 @@ class Combinaison{
     const Carte_clan* c1;
     const Carte_clan* c2;
     const Carte_clan* c3;
+    const Carte_clan* c4;
+
     int Puissance=0;
+    int nb_carte;
 
 public:
-    //const Carte& getCarte1() const { return *c1; }
-    //const Carte& getCarte2() const { return *c2; }
-    //const Carte& getCarte3() const { return *c3; }
+    const Carte_clan& getCarte1() const { return *c1; }
+    const Carte_clan& getCarte2() const { return *c2; }
+    const Carte_clan& getCarte3() const { return *c3; }
+    const Carte_clan& getCarte4() const { return *c4; }
+    
+    Combinaison(const Carte_clan* u, const Carte_clan* d, const Carte_clan* t): nb_carte(3), c1(u), c2(d), c3(t) {}
+    Combinaison(const Carte_clan* u, const Carte_clan* d, const Carte_clan* t, const Carte_clan* q): nb_carte(4), c1(u), c2(d), c3(t), c4(q) {}
+
     
     const int getPuissance() const {return Puissance;}
 
-    bool estUneCouleur();
-    bool estUneSuite();
-    bool estUnBrelan();
-    bool estUneSuiteCouleur();
+    bool estUneCouleur3();
+    bool estUneCouleur4();
+
+    bool estUneSuite3();
+    bool estUneSuite4();
+
+    bool estUnBrelan3();
+    bool estUnBrelan4();
+
+    bool estUneSuiteCouleur3();
+    bool estUneSuiteCouleur4();
 
     void PuissanceCombinaison();
 };
