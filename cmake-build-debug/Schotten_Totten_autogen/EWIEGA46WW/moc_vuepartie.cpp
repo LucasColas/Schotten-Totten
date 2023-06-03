4f75b6a80b4bef7c0d27532dc5ae9ae64bf8d691
lucascolas <lucas.colas13@gmail.com>
2023-06-03 08:46:47 +0200

    Cmake

--- cmake-build-debug/Schotten_Totten_autogen/EWIEGA46WW/moc_vuepartie.cpp
+++ cmake-build-debug/Schotten_Totten_autogen/EWIEGA46WW/moc_vuepartie.cpp
@@ -46,11 +46,14 @@ static constexpr auto qt_meta_stringdata_CLASSVuePartieENDCLASS = QtMocHelpers::
     "vc",
     "onPiocheClicked",
     "VuePioche*",
-    "p"
+    "p",
+    "onBorneClicked",
+    "VueBorne*",
+    "b"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSVuePartieENDCLASS_t {
-    uint offsetsAndSizes[16];
+    uint offsetsAndSizes[22];
     char stringdata0[10];
     char stringdata1[14];
     char stringdata2[1];
@@ -59,6 +62,9 @@ struct qt_meta_stringdata_CLASSVuePartieENDCLASS_t {
     char stringdata5[16];
     char stringdata6[11];
     char stringdata7[2];
+    char stringdata8[15];
+    char stringdata9[10];
+    char stringdata10[2];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSVuePartieENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -71,7 +77,10 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSVuePartieENDCLASS_t qt_meta_str
         QT_MOC_LITERAL(35, 2),  // "vc"
         QT_MOC_LITERAL(38, 15),  // "onPiocheClicked"
         QT_MOC_LITERAL(54, 10),  // "VuePioche*"
-        QT_MOC_LITERAL(65, 1)   // "p"
+        QT_MOC_LITERAL(65, 1),  // "p"
+        QT_MOC_LITERAL(67, 14),  // "onBorneClicked"
+        QT_MOC_LITERAL(82, 9),  // "VueBorne*"
+        QT_MOC_LITERAL(92, 1)   // "b"
     },
     "VuePartie",
     "onCardClicked",
@@ -80,7 +89,10 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSVuePartieENDCLASS_t qt_meta_str
     "vc",
     "onPiocheClicked",
     "VuePioche*",
-    "p"
+    "p",
+    "onBorneClicked",
+    "VueBorne*",
+    "b"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -92,7 +104,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSVuePartieENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -100,12 +112,14 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSVuePartieENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    1,   26,    2, 0x08,    1 /* Private */,
-       5,    1,   29,    2, 0x08,    3 /* Private */,
+       1,    1,   32,    2, 0x08,    1 /* Private */,
+       5,    1,   35,    2, 0x08,    3 /* Private */,
+       8,    1,   38,    2, 0x08,    5 /* Private */,
 
  // slots: parameters
     QMetaType::Void, 0x80000000 | 3,    4,
     QMetaType::Void, 0x80000000 | 6,    7,
+    QMetaType::Void, 0x80000000 | 9,   10,
 
        0        // eod
 };
@@ -124,7 +138,10 @@ Q_CONSTINIT const QMetaObject VuePartie::staticMetaObject = { {
         QtPrivate::TypeAndForceComplete<VueCarte *, std::false_type>,
         // method 'onPiocheClicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<VuePioche *, std::false_type>
+        QtPrivate::TypeAndForceComplete<VuePioche *, std::false_type>,
+        // method 'onBorneClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<VueBorne *, std::false_type>
     >,
     nullptr
 } };
@@ -137,6 +154,7 @@ void VuePartie::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
         switch (_id) {
         case 0: _t->onCardClicked((*reinterpret_cast< std::add_pointer_t<VueCarte*>>(_a[1]))); break;
         case 1: _t->onPiocheClicked((*reinterpret_cast< std::add_pointer_t<VuePioche*>>(_a[1]))); break;
+        case 2: _t->onBorneClicked((*reinterpret_cast< std::add_pointer_t<VueBorne*>>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
@@ -154,6 +172,13 @@ void VuePartie::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
             default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
             case 0:
                 *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< VuePioche* >(); break;
+            }
+            break;
+        case 2:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< VueBorne* >(); break;
             }
             break;
         }
@@ -179,13 +204,13 @@ int VuePartie::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
