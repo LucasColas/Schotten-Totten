3d28ee2194ec5c15786677609def56943712ed1c
Laurent MINATCHY <laurent.minatchy@etu.utc.fr>
2023-06-10 11:12:37 +0200

    Ajout de la defausse

---
+++ vuedefausse.cpp
@@ -0,0 +1,5 @@
+//
+// Created by laure on 10/06/2023.
+//
+
+#include "vuedefausse.h"
---
+++ vuedefausse.h
@@ -0,0 +1,16 @@
+//
+// Created by laure on 10/06/2023.
+//
+
+#ifndef SCHOTTEN_TOTTEN_VUEDEFAUSSE_H
+#define SCHOTTEN_TOTTEN_VUEDEFAUSSE_H
+
+
+
+class vuedefausse {
+
+};
+
+
+
+#endif //SCHOTTEN_TOTTEN_VUEDEFAUSSE_H
